30632ace392295e4de250ac7ee3fdb7c0e852584
Natanael <natanaelfb@hotmail.com>
2021-11-27 02:15:42 -0500

    tc6

---
+++ Dikstra.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC6
+{
+    class Dijkstra
+    {
+        private static int DistanciaMinima(int[] distancia, bool[] ConjuntoCaminoMasCorto, int TotalVertices)
+        {
+            int min = int.MaxValue;
+            int minIndex = 0;
+
+            for (int v = 0; v < TotalVertices; ++v)
+            {
+                if (ConjuntoCaminoMasCorto[v] == false && distancia[v] <= min)
+                {
+                    min = distancia[v];
+                    minIndex = v;
+                }
+            }
+
+            return minIndex;
+        }
+
+        private static void Imprimir(int[] distancia, int TotalVertices)
+        {
+            Console.WriteLine("Vértice    Distancia desde inicio");
+            string ruta="";
+            for (int i = 0; i < TotalVertices; ++i)
+            {
+                if (i == 0)
+                    ruta = "San miguel";
+                if (i == 1)
+                    ruta = "Callao";
+                if (i == 2)
+                    ruta = "San Borja";
+                if (i == 3)
+                    ruta = "La Molina";
+                if (i == 4)
+                    ruta = "Magdalena";
+
+                Console.WriteLine($"{i}{ruta}\t  {distancia[i]}", i, distancia[i]);
+            }
+        }
+
+        public static void DijkstraAlgo(int[,] grafo, int Inicio, int TotalVertices)
+        {
+            int[] distancia = new int[TotalVertices];
+            bool[] ConjuntoCaminoMasCorto = new bool[TotalVertices];
+
+            for (int i = 0; i < TotalVertices; ++i)
+            {
+                distancia[i] = int.MaxValue;
+                ConjuntoCaminoMasCorto[i] = false;
+            }
+
+            distancia[Inicio] = 0;
+
+            for (int count = 0; count < TotalVertices - 1; ++count)
+            {
+                int u = DistanciaMinima(distancia, ConjuntoCaminoMasCorto, TotalVertices);
+                ConjuntoCaminoMasCorto[u] = true;
+
+                for (int v = 0; v < TotalVertices; ++v)
+                    if (!ConjuntoCaminoMasCorto[v] && Convert.ToBoolean(grafo[u, v]) &&
+                        distancia[u] != int.MaxValue && distancia[u] + grafo[u, v] < distancia[v])
+                        distancia[v] = distancia[u] + grafo[u, v];
+            }
+
+            Imprimir(distancia, TotalVertices);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             *  san miguel -> San miguel 0
+             */
+            int[,] graph =  {
+                          { 0, 6,0,8,0 },
+                          { 0,0,16,0,0},
+                          { 8,0,0,10,0},
+                          { 0,0,0,0,9},
+                          { 0,1,0,0,0}
+            };
+
+            //El camino más corto de Dijkstra
+            Dijkstra.DijkstraAlgo(graph, 1, 5);
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
